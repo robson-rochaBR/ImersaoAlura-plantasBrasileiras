8291954b07f3cd4fc53d0293270654c967569a2e
Robson Rocha <robsonrocha254@yahoo.com>
2024-11-08 15:56:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,45 @@
+let dados = [
+    {
+        titulo: "Ipê-Amarelo: Um Brilho Dourado na Natureza",
+        descricao: "O ipê-amarelo é uma árvore brasileira de grande beleza e importância ecológica. Conhecida por suas flores exuberantes de um amarelo intenso, que florescem principalmente no final do inverno e início da primavera, essa espécie encanta a todos com sua exuberância. Além de sua beleza ornamental, o ipê-amarelo desempenha um papel fundamental na natureza, fornecendo alimento e abrigo para diversas espécies de animais, além de contribuir para a conservação do solo e do meio ambiente.",
+        imagem: "/image/ipe.jpg",
+        link: "https://pt.wikipedia.org/wiki/Ip%C3%AA-amarelo-flor-de-algod%C3%A3o",
+        tags: "ipe"
+    },
+    {
+        titulo: "Romã: Um Fruto Vermelho e Delicioso",
+        descricao: "A romã é uma fruta tropical conhecida por sua casca grossa e sementes vermelhas envoltas em um suco adocicado e azedinho. Além de ser deliciosa, a romã é rica em antioxidantes e possui diversas propriedades medicinais. A romãzeira, árvore que produz a fruta, é apreciada por sua beleza e resistência.",
+        imagem: "/image/roma.jpg",
+        link: "https://pt.wikipedia.org/wiki/Rom%C3%A",
+        tags: "roma Roma"
+    },
+    {
+        titulo: "Albízia: A Beleza Delicada e Sombra Agradável",
+        descricao: "A Albízia é uma árvore ornamental conhecida por suas folhas delicadas que se fecham à noite e suas flores perfumadas em forma de pompons. É uma árvore de crescimento rápido e proporciona uma sombra densa, sendo ideal para jardins e parques. Além disso, a Albízia possui propriedades medicinais e é utilizada na produção de madeira.",
+        imagem: "/image/albizia.jpg",
+        link: "https://pt.wikipedia.org/wiki/Albizia",
+        tags: "albizia albízia"
+    },
+    {
+        titulo: "Carnaúba",
+        descricao: "A carnaúba é uma palmeira emblemática do Nordeste brasileiro, conhecida por sua cera extraída de suas folhas, utilizada em diversos produtos como cosméticos e polidores. É uma espécie fundamental para o ecossistema da região.",
+        imagem: "/image/carnauba.jpg",
+        link: "https://pt.wikipedia.org/wiki/Carna%C3%BAba",
+        tags: "carnaúba carnauba"
+    },
+    {
+        titulo: "Mandacaru",
+        descricao: "O mandacaru é um cacto típico da Caatinga, adaptado às condições áridas do Nordeste. Ele possui longos espinhos e flores brancas que se abrem à noite. É uma planta resistente e símbolo da região.",
+        imagem: "/image/mandacaru.jpg",
+        link: "https://pt.wikipedia.org/wiki/Mandacaru",
+        tags: ""
+    },
+    {
+        titulo: "Umbuzeiro",
+        descricao: "O umbuzeiro é uma árvore frutífera muito importante para o semiárido nordestino. Seus frutos, os umbus, são ricos em vitamina C e são utilizados na culinária local. A árvore também fornece sombra e abrigo para diversos animais.",
+        imagem: "/image/umbuzeiro.jpg",
+        link: "https://pt.wikipedia.org/wiki/Umbuzeiro",
+        tags: ""
+    }
+];
+
